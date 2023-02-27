01223a7fcb8e81df334b6efc1e8c2035f78db734
yudaiuk <yuudaiufo@icloud.com>
2023-02-27 11:06:02 +0900

    analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import VueHead from 'vue-head'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import VueHead from "vue-head";
+import VueGtag from "vue-gtag";
 
-createApp(App).use(router, VueHead).mount('#app')
+createApp(App)
+  .use(router, VueHead, VueGtag, {
+    config: {
+      id: "G-QXYK457BGN",
+    },
+  })
+  .mount("#app");
