5ca9287697258ef61c1809d0f2cd233775d0bd24
yudaiuk <yuudaiufo@icloud.com>
2023-02-28 18:19:43 +0900

    トップ画像変更

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,9 @@
 </template>
 
 <script>
-/* import HelloWorld from './components/HelloWorld.vue' */
-/* import HeadMessage from './components/HeadMessage.vue'
-import NewRacing from './components/NewRacing.vue'
-import Prefecture from './components/Prefecture.vue' */
-
 export default {
-  name: 'App',
-  components: {
-    /* HelloWorld, */
-    /* HeadMessage,
-    NewRacing,
-    Prefecture */
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
@@ -26,6 +15,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/page/top/HeadMessage.vue
+++ src/page/top/HeadMessage.vue
@@ -1,20 +1,40 @@
 <template>
-  <h1 class="header-message">
-    峠行きたい
-  </h1>
+  <div class="main-fv">
+    <h1 class="header-message">峠イキタイ</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HeadMessage',
-}
+  name: "HeadMessage",
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 .header-message {
   font-weight: bold;
   font-size: 30px;
-  height: 40vh;
+  padding-top: calc(50vh / 2 - 1em / 2);
+  margin: 0;
+}
+
+.main-fv {
+  height: 50vh;
+  position: relative;
+  background: rgba(255, 255, 255, 0.7);
+
+  &:before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    display: block;
+    background-image: url(~@/assets/top/main-fv.webp);
+    background-size: cover;
+    background-position: center;
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 0;
+  }
 }
 </style>
